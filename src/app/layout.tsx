5b19309cf27c143dd2161fb3f239522b18ba10ec
김기태 <kku98057@naver.com>
2024-11-03 22:39:58 +0900

    fix.robots.txt, sitemap,네이버서치어드바이저 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,27 @@ export const metadata: Metadata = {
   title: "밸런썸 (Balancesome)",
   description:
     "밸런썸 (Balancesome) 다양한 게임을 만들고 즐기고 공유하고 랭킹을 확인하세요!",
+  // openGraph: {
+  //   title: '토너먼트 게임',
+  //   description: '토너먼트 게임 플랫폼',
+  //   url: 'https://your-domain.com',
+  //   siteName: '토너먼트 게임',
+  //   images: [
+  //     {
+  //       url: 'https://your-domain.com/og-image.jpg',
+  //       width: 1200,
+  //       height: 630,
+  //     },
+  //   ],
+  //   locale: 'ko_KR',
+  //   type: 'website',
+  // },
+  // twitter: {
+  //   card: 'summary_large_image',
+  //   title: '토너먼트 게임',
+  //   description: '토너먼트 게임 플랫폼',
+  //   images: ['https://your-domain.com/og-image.jpg'],
+  // },
 };
 
 export default function RootLayout({
@@ -23,6 +44,10 @@ export default function RootLayout({
           as="style"
           crossOrigin="anonymous"
           href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/variable/pretendardvariable-dynamic-subset.min.css"
+        />
+        <meta
+          name="naver-site-verification"
+          content="5bf1ec7d9b1db9137927d7f2f151d81a7885b897"
         />
       </head>
       <body>
