f141a389c49bce557c45a5d2848408b06ac417a1
김기태 <kku98057@naver.com>
2024-11-16 15:30:58 +0900

    feat.밸런스게임에 광고추가

--- src/app/_components/adsense/GoogleAdSenseComponent.tsx
+++ src/app/_components/adsense/GoogleAdSenseComponent.tsx
@@ -9,17 +9,27 @@ export default function GoogleAdSenseComponent() {
     (window.adsbygoogle = window.adsbygoogle || []).push({});
   }, []);
   return (
-    <div className="my-2 max-w-[500px] mx-auto relative overflow-hidden">
+    <div className="my-2 ">
       {" "}
       {/* 컨테이너 크기 제한 */}
-      <ins
+      {/* <ins
         className="adsbygoogle"
         style={{ display: "block" }}
         data-ad-client="ca-pub-3604338976078798"
         data-ad-slot="4815843481"
         data-ad-format="auto"
-        data-full-width-responsive="true"
+        data-full-width-responsive="false"
         data-adtest="on"
+      ></ins> */}
+      <ins
+        className="adsbygoogle"
+        style={{
+          display: "inline-block",
+          width: "250px",
+          height: "90px",
+        }}
+        data-ad-client="ca-pub-3604338976078798"
+        data-ad-slot="4815843481"
       ></ins>
     </div>
   );
