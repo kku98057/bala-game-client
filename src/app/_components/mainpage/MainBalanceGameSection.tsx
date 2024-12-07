e4ae84c0f446ec4485ed62e1b0b22bfb01ce2ac4
김기태 <kku98057@naver.com>
2024-12-08 01:58:21 +0900

    fix.queryFn추가

--- src/app/_components/mainpage/MainBalanceGameSection.tsx
+++ src/app/_components/mainpage/MainBalanceGameSection.tsx
@@ -3,11 +3,14 @@ import Container from "../Container";
 import { dehydrate, Hydrate, QueryClient } from "@tanstack/react-query";
 import { QUERYKEYS } from "@/queryKeys";
 import BalanceGameList from "./_components/MainBalanceGameList";
+import getBalanceGameListData from "@/app/game/balanceGame/_lib/getBalanceGameListData";
 
 export default async function MainBalanceGameSection() {
   const queryClient = new QueryClient();
   await queryClient.prefetchQuery({
     queryKey: QUERYKEYS.balanceGame.lists({ limit: 4, sort: "popular" }),
+    queryFn: () =>
+      getBalanceGameListData({ limit: 4, sort: "popular", page: 1 }),
   });
   const dehydratedState = dehydrate(queryClient);
   return (
